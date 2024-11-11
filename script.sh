0ce46e06e6a39b6ee61490cdca11a92d5b343a8a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-11 23:23:55 +0600

    Huh

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ echo "Sync success"
 echo "============="
 
 # Export
-export BUILD_USERNAME=FARHAN 
+export BUILD_USERNAME=devil
 export BUILD_HOSTNAME=crave 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
