a63bb82bb025bc16a57e0e73c69c5e296759e2a9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-11 23:21:05 +0600

    Huh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b 14/infinity .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Infinity-x .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
