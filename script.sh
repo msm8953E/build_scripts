85ad951bf9d9debb6bba2de847b8634e557d763c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-12-09 07:02:41 +0600

    Ok

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b Infinity-x .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b InfinityX/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -21,7 +21,7 @@ echo "Sync success"
 echo "============="
 
 # Export
-export BUILD_USERNAME=devil 
+export BUILD_USERNAME=FARHAN 
 export BUILD_HOSTNAME=crave 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
