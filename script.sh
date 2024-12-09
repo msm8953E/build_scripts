26ed167055070b0e2cb780e10bb48feac33b3fc9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-12-09 07:07:01 +0600

    Update

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/Trijal08/project_infinity_x-manifest -b 15 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
