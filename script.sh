90f942d243dc45fe65a1ea4a3f86f5d13838482b
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-12-10 18:10:20 +0600

    Add

--- script.sh
+++ script.sh
@@ -4,22 +4,21 @@ rm -rf .repo/local_manifests/
 
 # repo init rom
 repo init -u https://github.com/VoltageOS/manifest.git -b 15 --git-lfs
-        
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
+# repo sync
+/opt/crave/resync.sh && repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+echo "============="
+echo "Sync success"
+echo "============="
+       
 # Local manifests
 git clone https://github.com/Gtajisan/local_manifests_clo -b Voltage/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
-
-# Sync
-/opt/crave/resync.sh && repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-echo "============="
-echo "Sync success"
-echo "============="
 
 # Export
 export BUILD_USERNAME=Farhan 
@@ -32,11 +31,10 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 . build/envsetup.sh
-brunch mi439 userdebug
-breakfast mi439
 lunch voltage_mi439-userdebug ||  lunch voltage_mi439-ap2a-userdebug || lunch voltage_mi439-ap3a-userdebug 
 mka bacon
 # wt
 . build/envsetup.sh
+brunch mi439 userdebug
 brunch mi439
-
+breakfast mi439
