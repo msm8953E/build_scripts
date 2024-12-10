c1ccce980b8629851c9a6a871cc2cc25a668f530
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-12-10 18:01:19 +0600

    Add

--- script.sh
+++ script.sh
@@ -32,10 +32,11 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 . build/envsetup.sh
+brunch mi439 userdebug
+breakfast mi439
 lunch voltage_mi439-userdebug ||  lunch voltage_mi439-ap2a-userdebug || lunch voltage_mi439-ap3a-userdebug 
 mka bacon
 # wt
 . build/envsetup.sh
-brunch mi439 userdebug
 brunch mi439
-breakfast mi439
+
