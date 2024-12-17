e5ef52c48556d83d374297c7a8bebe2967588f83
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-12-17 23:19:32 +0600

    A15

--- script.sh
+++ script.sh
@@ -3,13 +3,14 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
+#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b infinity .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b Infinity_15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
