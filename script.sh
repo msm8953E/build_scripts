f1d8abad89f81b7c6fe294809578b0a009681365
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-01-25 05:23:06 +0600

    Let's see

--- script.sh
+++ script.sh
@@ -3,9 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
-#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
-
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -26,13 +24,13 @@ echo "============="
 #rm -rf packages/apps/FaceUnlock
 
 # play vendor_infinity-priv_keys
-git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
-cd vendor/infinity-priv/keys
-./keys.sh
-cd ../../../
-echo "========================="
-echo "vendor_infinity-priv_keys"
-echo "========================="
+#git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+#cd vendor/infinity-priv/keys
+#./keys.sh
+#cd ../../../
+#echo "========================="
+#echo "vendor_infinity-priv_keys"
+#echo "========================="
 
 # Export
 export BUILD_USERNAME=FARHAN
