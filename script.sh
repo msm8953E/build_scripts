88f42f82c588ce46e20fa6e353d0819ab8b35654
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-07 11:51:39 +0600

    Fix

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ echo "=================="
 
 # Local manifests
 git clone https://github.com/msm8953E/local_manifests -b infinity/14x .repo/local_manifests
-git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b lineage-21 device/xiaomi/daisy
+git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b infinity/14 device/xiaomi/daisy
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
