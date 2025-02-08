c53bb4b8be158b3c6da69b81fe77ac1c035005c2
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-09 01:48:08 +0600

    Biker

--- script.sh
+++ script.sh
@@ -35,7 +35,7 @@ echo "vendor_infinity-priv_keys"
 echo "========================="
 
 # Export
-export BUILD_USERNAME=FARHAN
+export BUILD_USERNAME=beaker 
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
