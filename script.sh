814771f633880a0a3452ffdd41ae5192abd6cc2e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-13 00:18:15 +0600

    A

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ rm -rf android_device_xiaomi_daisy
 
 
 # repo init rom
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR3 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -25,17 +25,20 @@ echo "============="
 # remove face unlock 
 #rm -rf packages/apps/FaceUnlock
 
+# repo
+git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b infinity/15 device/xiaomi/daisy
+
 # play vendor_infinity-priv_keys
-#git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
-#cd vendor/infinity-priv/keys
-#./keys.sh
-#cd ../../../
+git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+cd vendor/infinity-priv/keys
+./keys.sh
+cd ../../../
 echo "========================="
 echo "vendor_infinity-priv_keys"
 echo "========================="
 
 # Export
-export BUILD_USERNAME=beaker 
+export BUILD_USERNAME=FARHAN 
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
