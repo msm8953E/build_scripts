553ff422fcda90be10e62ae8892841648cd3398c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-05 02:00:58 +0600

    Evo

--- script.sh
+++ script.sh
@@ -5,7 +5,8 @@ rm -rf android_device_xiaomi_daisy
 
 
 # repo init rom
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+#repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
