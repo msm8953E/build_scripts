315b9e45da175f9a346f9577bf590b99d04c0023
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-06 09:14:47 +0600

    Created

---
+++ Exp.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+rm -rf android_device_xiaomi_daisy
+
+
+# repo init rom
+#repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/msm8953E/device_xiaomi_sakura.git -b lineage-21 device/xiaomi/daisy
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# remove face unlock 
+#rm -rf packages/apps/FaceUnlock
+
+# play vendor_infinity-priv_keys
+#git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+#cd vendor/infinity-priv/keys
+#./keys.sh
+#cd ../../../
+echo "========================="
+echo "vendor_infinity-priv_keys"
+echo "========================="
+
+# Export
+export BUILD_USERNAME=ome 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+echo "======= Export Done ======"
+
+# aging 
+rm -rf device/xiaomi/daisy
+git clone https://github.com/msm8953E/device_xiaomi_sakura.git -b lineage-21 device/xiaomi/daisy
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+. build/envsetup.sh
+lunch lineage_sakura-userdebug
+lunch lineage_sakura-ap4a-userdebug
+brunch sakura
+
