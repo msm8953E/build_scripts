ddb2bacfa6a96ff353d5d4dc3731593d1cb93a94
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-27 12:48:40 +0600

    Nobob

--- script.sh
+++ script.sh
@@ -35,7 +35,7 @@ echo "vendor_infinity-priv_keys"
 echo "========================="
 
 # Export
-export BUILD_USERNAME=achu 
+export BUILD_USERNAME=nkbob 
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
