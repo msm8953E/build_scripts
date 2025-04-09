4afdf6a115540a96bff4d238fa699a6e0499af3c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-04-09 11:43:32 +0600

    Removed

--- script.sh
+++ script.sh
@@ -30,13 +30,13 @@ rm -rf device/xiaomi/daisy
 git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b infinity/14 device/xiaomi/daisy
 
 # play vendor_infinity-priv_keys
-git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
-cd vendor/infinity-priv/keys
-./keys.sh
-cd ../../../
-echo "========================="
-echo "vendor_infinity-priv_keys"
-echo "========================="
+#git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+#cd vendor/infinity-priv/keys
+#./keys.sh
+#cd ../../../
+#echo "========================="
+#echo "vendor_infinity-priv_keys"
+#echo "========================="
 
 # Export
 export BUILD_USERNAME=achu 
