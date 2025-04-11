2d95a8d776167bc8c7d9354da6b417b025f1de36
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-04-11 18:43:02 +0600

    Add

--- script.sh
+++ script.sh
@@ -6,14 +6,14 @@ rm -rf android_device_xiaomi_daisy
 
 # repo init rom
 #repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
 
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b cr/14 device/xiaomi/daisy
+git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b Qpr2 device/xiaomi/daisy
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -45,7 +45,7 @@ cd ../../..
 
 # aging 
 rm -rf device/xiaomi/daisy
-git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b cr/14 device/xiaomi/daisy
+git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b Qpr2 device/xiaomi/daisy
 
 # Set up build environment
 source build/envsetup.sh
