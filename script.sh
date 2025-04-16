45eb31fcca9fbe4df7a0d63945976de22f642a31
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-04-16 16:25:46 +0600

    Dix*

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b Evo/15 device/xiaomi/daisy
+git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b Evo/15-qpr2 device/xiaomi/daisy
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -27,9 +27,9 @@ echo "============="
 #rm -rf packages/apps/FaceUnlock
 
 # play vendor_infinity-priv_keys
-git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-cd vendor/evolution-priv/keys
-./keys.sh
+#git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+#cd vendor/evolution-priv/keys
+#./keys.sh
 cd ../../..
 echo "========================="
 echo "vendor_evo-priv_keys"
@@ -44,7 +44,7 @@ echo "======= Export Done ======"
 
 # aging 
 rm -rf device/xiaomi/daisy
-git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b Evo/15 device/xiaomi/daisy
+git clone https://github.com/msm8953E/android_device_xiaomi_daisy -b Evo/15-qpr2 device/xiaomi/daisy
 
 # Set up build environment
 source build/envsetup.sh
