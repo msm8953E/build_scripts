eea7f83c6f7aa706cfc47a80c771c28c9f136578
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-05-10 11:42:01 +0600

    Added achu*

--- script.sh
+++ script.sh
@@ -44,7 +44,7 @@ echo "vendor_evo-priv_keys"
 echo "========================="
 
 # Export
-export BUILD_USERNAME=ome 
+export BUILD_USERNAME=achu 
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
