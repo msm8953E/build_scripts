65119872d1976cda72147713aa03002a1a3ec7d1
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-09-21 23:16:22 +0600

    Added

--- Mist15.sh
+++ Mist15.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests/
+#rm -rf .repo/local_manifests/
+#rm -rf android_device_xiaomi_daisy
 
-# repo init rom
-repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
+
+# Initialize local repository
+#repo init -u https://github.com/Project-Mist-OS/manifest -b vic --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b MIST/15 .repo/local_manifests
+#git clone https://github.com/msm8953F/android_device_xiaomi_daisy -b Alpha-15 device/xiaomi/daisy
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# Sync
-/opt/crave/resync.sh
+# build
+#/opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-# keys
-# mkdir vendor/lineage-priv
-# cp build-keys/* vendor/lineage-priv
-# echo "============="
-# echo "Keys copied"
-# echo "============="
+# remove face unlock 
+#rm -rf packages/apps/FaceUnlock
 
 # Export
-export BUILD_USERNAME=FARHAN 
-export BUILD_HOSTNAME=crave
+export BUILD_USERNAME=achu 
+export BUILD_HOSTNAME=crave_x_shaaim
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 echo "======= Export Done ======"
+
+# aging 
+#rm -rf packages/apps/Settings
+#git clone https://github.com/frnwot/android_packages_apps_Settings_crd.git -b 15.0 packages/apps/Settings
+#rm -rf device/xiaomi/daisy
+#git clone https://github.com/frnwot/android_device_xiaomi_daisy_standard -b mist-15 device/xiaomi/daisy
 
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-# lunch 1
-. build/envsetup.sh
-brunch Mi439_4_19 userdebug
-brunch Mi439_4_19
-
 # Lunch
-mistify Mi439_4_19 userdebug
-mist b
-
+. build/envsetup.sh
+mistify daisy user 
+mist sb
 
